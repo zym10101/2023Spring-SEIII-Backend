efc63356ae59a66c76b4f4a311f5f8ce58c2bfb7
syf <201250009@smail.nju.edu.cn>
2023-05-28 13:48:45 +0800

    情绪文本占比波动图

--- service/data_analysis/get_issue_senti_pct.py
+++ service/data_analysis/get_issue_senti_pct.py
@@ -1,4 +1,6 @@
 from dao.IssueDao import get_by_create_time_all
+import pandas as pd
+import plotly.graph_objs as go
 
 
 def get_pct(repo_name, start_time, end_time, polarity):
@@ -40,3 +42,38 @@ def get_issue_neg_pct(repo_name, start_time, end_time):
         return f"该时间段内，{repo_name} issue为空！"
     else:
         return pct
+
+
+# interval格式: “xxD”，如”60D“表示60天
+def plot_pct_over_time(repo_name, start_time, end_time, interval):
+    if get_issue_pos_pct(repo_name, start_time, end_time) != f"该时间段内，{repo_name} issue为空！":
+        # 准备数据
+        pos_pct = pd.Series(get_issue_pos_pct(repo_name, start_time, end_time))
+        neg_pct = pd.Series(get_issue_neg_pct(repo_name, start_time, end_time))
+        index = pd.date_range(start_time, end_time, freq=interval)
+
+        # 创建两条折线，基计得分和消极得分
+        trace1 = go.Scatter(
+            x=index,
+            y=pos_pct,
+            mode='lines',
+            name='积极文本占比'
+        )
+
+        trace2 = go.Scatter(
+            x=index,
+            y=neg_pct,
+            mode='lines',
+            name='消极文本占比'
+        )
+
+        # 设置图表布局
+        layout = go.Layout(
+            title='情绪文本占比波动图',
+            xaxis=dict(title='Date'),
+            yaxis=dict(title='Score')
+        )
+
+        # 绘制图表
+        fig = go.Figure(data=[trace1, trace2], layout=layout)
+        fig.show()
