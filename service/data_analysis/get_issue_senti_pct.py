d9e83ed2bf461710f266d1090cfe4294ddf49588
Yunfei Zhang <201250022@smail.nju.edu.cn>
2023-05-28 18:11:53 +0800

    modify files structure

---
+++ service/data_analysis/get_issue_senti_pct.py
@@ -0,0 +1,42 @@
+from dao.IssueDao import get_by_create_time_all
+
+
+def get_pct(repo_name, start_time, end_time, polarity):
+    issues = get_by_create_time_all(repo_name, start_time, end_time)
+
+    if len(issues) == 0:
+        return f"该时间段内，{repo_name} issue为空！"
+
+    pos_count = 0
+    neg_count = 0
+    neutral_count = 0
+
+    for issue in issues:
+        result = int(issue.pos_body) + int(issue.neg_body)
+        if result > 0:
+            pos_count = pos_count + 1
+        elif result == 0:
+            neutral_count = neutral_count + 1
+        else:
+            neg_count = neg_count + 1
+
+    if polarity == "pos":
+        return pos_count / len(issues)
+    else:
+        return neg_count / len(issues)
+
+
+def get_issue_pos_pct(repo_name, start_time, end_time):
+    pct = get_pct(repo_name, start_time, end_time, "pos")
+    if pct.startswith("该时间段内"):
+        return f"该时间段内，{repo_name} issue为空！"
+    else:
+        return pct
+
+
+def get_issue_neg_pct(repo_name, start_time, end_time):
+    pct = get_pct(repo_name, start_time, end_time, "neg")
+    if pct.startswith("该时间段内"):
+        return f"该时间段内，{repo_name} issue为空！"
+    else:
+        return pct
--- service/data_analysis/repo_gen_pct_change.py
+++ service/data_analysis/repo_gen_pct_change.py
@@ -1,49 +1,7 @@
-from dao.IssueDao import get_by_create_time_all
-import pandas as pd
 import plotly.graph_objs as go
+from service.data_analysis.get_issue_senti_pct import get_issue_pos_pct, get_issue_neg_pct
 import datetime
 import math
-
-
-def get_pct(repo_name, start_time, end_time, polarity):
-    issues = get_by_create_time_all(repo_name, start_time, end_time)
-
-    if len(issues) == 0:
-        return f"该时间段内，{repo_name} issue为空！"
-
-    pos_count = 0
-    neg_count = 0
-    neutral_count = 0
-
-    for issue in issues:
-        result = int(issue.pos_body) + int(issue.neg_body)
-        if result > 0:
-            pos_count = pos_count + 1
-        elif result == 0:
-            neutral_count = neutral_count + 1
-        else:
-            neg_count = neg_count + 1
-
-    if polarity == "pos":
-        return pos_count / len(issues)
-    else:
-        return neg_count / len(issues)
-
-
-def get_issue_pos_pct(repo_name, start_time, end_time):
-    pct = get_pct(repo_name, start_time, end_time, "pos")
-    if pct.startswith("该时间段内"):
-        return f"该时间段内，{repo_name} issue为空！"
-    else:
-        return pct
-
-
-def get_issue_neg_pct(repo_name, start_time, end_time):
-    pct = get_pct(repo_name, start_time, end_time, "neg")
-    if pct.startswith("该时间段内"):
-        return f"该时间段内，{repo_name} issue为空！"
-    else:
-        return pct
 
 
 def plot_pct_over_time(repo_name, start_time, end_time):
