5b5f6455ce4fb8fa2974d13ad3182f73066cc9cc
Yunfei Zhang <201250022@smail.nju.edu.cn>
2023-05-29 19:46:08 +0800

    basically finish plot senti pct by reaction

---
+++ service/data_analysis/get_senti_pct_by_reaction.py
@@ -0,0 +1,6 @@
+def get_issue_senti_pct_by_reaction(repo, reaction, start_time, end_time, polarity):
+    return []
+
+
+def get_comment_senti_pct_by_reaction(repo, reaction, start_time, end_time, polarity):
+    return []
