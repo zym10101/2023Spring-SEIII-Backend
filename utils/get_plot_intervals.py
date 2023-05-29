93f4a59d563aca70471f57ea2c7d97f01b76a937
Yunfei Zhang <201250022@smail.nju.edu.cn>
2023-05-29 21:23:33 +0800

    fix: 防御式获取间隔；init user plot

--- utils/get_plot_intervals.py
+++ utils/get_plot_intervals.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from datetime import datetime
+
 
 # 生成绘图的时间间隔
 #
@@ -9,12 +11,20 @@ import pandas as pd
 #
 # 返回表示时间间隔的datetime对象列表。
 def get_plot_intervals(start_time, end_time, freq=None, periods=8):
+    date1 = datetime.strptime(start_time, '%Y-%m-%d')
+    date2 = datetime.strptime(end_time, '%Y-%m-%d')
+    delta = date2 - date1
+    delta_days = delta.days
+    result_str = '{}D'.format(delta_days)
+    if int(result_str[:-1]) < 8:
+        freq = '1D'
     if freq is not None:
         intervals = pd.date_range(start=start_time, end=end_time, freq=freq).tolist()
     else:
         intervals = pd.date_range(start=start_time, end=end_time, periods=periods+1).tolist()
     dates = pd.DatetimeIndex(intervals).date.tolist()
     return [d.strftime('%Y-%m-%d') for d in dates]
+
 
 if __name__ == '__main__':
     print(get_plot_intervals('2022-01-01', '2022-01-09'))
