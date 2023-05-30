a5bac928ebdd46ab1267d7ea5ae3149bbce92296
Yunfei Zhang <201250022@smail.nju.edu.cn>
2023-05-30 15:18:15 +0800

    modify user dao

--- dao/UserDao.py
+++ dao/UserDao.py
@@ -11,16 +11,21 @@ def get_by_id(id):
     return User.query.get(id)
 
 
-def get_all_issues_and_comments_by_id(id):
+def get_issues_and_by_id(id):
     user = get_by_id(id)
-    return user.comment, user.issue
+    return user.issue
+
+
+def get_commenys_by_id(id):
+    user = get_by_id(id)
+    return user.comment
 
 
 if __name__ == '__main__':
     from app_template import app
     from datetime import datetime
 
-    with app.app_context():
-        uid = 40265686
-        comments, issues = get_all_issues_and_comments_by_id(uid)
-        print(1)
+    # with app.app_context():
+    #     uid = 40265686
+    #     comments, issues = get_all_issues_and_comments_by_id(uid)
+    #     print(1)
