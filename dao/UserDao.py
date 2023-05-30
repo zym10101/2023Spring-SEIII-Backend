65de505b7aef19fa2ad70030881da688ec40e0c5
Yujie Song <201870189@smail.nju.edu.cn>
2023-05-30 11:05:09 +0800

    新增获取user所有issue和comment的dao层接口

---
+++ dao/UserDao.py
@@ -0,0 +1,26 @@
+from sqlalchemy import *
+
+from dao.Database import db
+from model.User import User
+from model.Issue import Issue
+from model.Label import Label
+from model.Comment import Comment
+
+
+def get_by_id(id):
+    return User.query.get(id)
+
+
+def get_all_issues_and_comments_by_id(id):
+    user = get_by_id(id)
+    return user.comment, user.issue
+
+
+if __name__ == '__main__':
+    from app_template import app
+    from datetime import datetime
+
+    with app.app_context():
+        uid = 40265686
+        comments, issues = get_all_issues_and_comments_by_id(uid)
+        print(1)
