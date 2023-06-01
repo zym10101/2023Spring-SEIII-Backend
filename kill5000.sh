53a2e2721fb161398a57d2df8aa0682f9df44119
Yujie Song <201870189@smail.nju.edu.cn>
2023-06-01 19:31:52 +0800

    杀死之前占用5000端口的程序

---
+++ kill5000.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+port=5000
+
+process_ids=$(lsof -t -i :$port)
+if [ -n "$process_ids" ]; then
+    echo "Port $port is already in use. Killing the process(es)..."
+    kill $process_ids
+    echo "Process(es) on port $port killed successfully."
+else
+    echo "No processes found on port $port."
+fi
