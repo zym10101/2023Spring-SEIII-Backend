2896671c1b10a1d6c91ec05c16969ddf82faacf9
201250015 <201250015@smail.nju.edu.cn>
2023-06-02 12:59:11 +0800

    finish: 若labels大于25个，只返回前25个

--- dao/LabelDao.py
+++ dao/LabelDao.py
@@ -6,17 +6,39 @@ from model.Label import Label
 
 
 def get_labels(repo_name, begin_time, end_time):
-    labels_list = [issue.labels for issue in Issue.query \
+    issues = Issue.query \
         .filter(Issue.repository_url.endswith(repo_name)) \
         .filter(Issue.created_at.between(begin_time, end_time)) \
-        .all()]
-    labels = list(set(element for sublist in labels_list for element in sublist))
-    label_id = [label.id for label in labels]
-    if len(label_id)==0:
+        .all()
+    labels_list = [issue.labels for issue in issues]
+
+    if len(labels_list)==0:
         return []
-    labels = Label.query.filter(or_(*[Label.id == id for id in label_id])).with_entities(Label.name).all()
-    label_names = list(set([label.name for label in labels]))
-    return label_names
+
+    labels = list(set(element for sublist in labels_list for element in sublist))
+
+    if len(labels)>25:
+        label_count = {}
+        for issue in issues:
+            for label in issue.labels:
+                if label.id in label_count:
+                    label_count[label.id] += 1
+                else:
+                    label_count[label.id] = 1
+
+        if len(label_count) > 25:
+            sorted_labels = sorted(label_count.items(), key=lambda x: x[1], reverse=True)[:25]
+        else:
+            sorted_labels = sorted(label_count.items(), key=lambda x: x[1], reverse=True)
+
+        top_labels = [Label.query.get(label_id) for label_id, count in sorted_labels]
+        return list(set([label.name for label in top_labels]))
+    else:
+        label_id = [label.id for label in labels]
+        labels = Label.query.filter(or_(*[Label.id == id for id in label_id])).with_entities(Label.name).all()
+        label_names = list(set([label.name for label in labels]))
+        return label_names
+
 
 
 def get_labels_8(repo_name, begin_time, end_time):
